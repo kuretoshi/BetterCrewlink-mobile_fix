510b457cd6bc35ea7fbdb1a61008dfa605ba8c22
くれとし <tamo-toshi5819@hotmail.co.jp>
2026-06-14 15:41:36 +0900

    Lower remote cosmetics further

--- src/app/compontents/avatar/avatar.component.ts
+++ src/app/compontents/avatar/avatar.component.ts
@@ -6,7 +6,7 @@ import { SettingsService } from '../../services/settings.service';
 const HAT_COLLECTION_URL = 'https://cdn.jsdelivr.net/gh/OhMyGuus/BetterCrewLink-Hats@master/';
 const MOBILE_AVATAR_TOP_OFFSET = '14%';
 const MOBILE_SKIN_TOP_OFFSET = '22%';
-const MOBILE_COSMETIC_Y_OFFSET = '8%';
+const MOBILE_COSMETIC_Y_OFFSET = '13%';
 
 interface CosmeticData {
 	image?: string;
